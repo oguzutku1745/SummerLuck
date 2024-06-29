a1b3087e625cc96b75c6d6b1e42446d167f1d245
Oğuz Utku Yıldız <oguzutku1745@gmail.com>
2024-06-29 21:15:50 +0200

    Funny commits didn't work, changing .env

--- app/api/render-image.tsx
+++ app/api/render-image.tsx
@@ -4,45 +4,40 @@ export const config = {
   runtime: "edge",
 };
 
-export default async function handler(req:any) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const page = searchParams.get("page") || "initial";
-    const transactionId = searchParams.get("transactionId");
-    const isFollowing = searchParams.get("isFollowing") === "true";
+export default async function handler(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const page = searchParams.get("page") || "initial";
+  const transactionId = searchParams.get("transactionId");
+  const isFollowing = searchParams.get("isFollowing") === "true";
 
-    let content;
+  let content;
 
-    if (page === "initial") {
-      content = (
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", backgroundColor: "#800080", color: "#fff", fontSize: 40 }}>
-          Welcome to Lucky Summer!
-          You can join the raffle if you follow the caster.
-        </div>
-      );
-    } else if (page === "result") {
-      content = (
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", backgroundColor: "#800080", color: "#fff", fontSize: 40 }}>
-          Transaction submitted! {transactionId}
-        </div>
-      );
-    } else {
-      content = (
-        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", backgroundColor: "#800080", color: "#fff", fontSize: 40 }}>
-          {isFollowing ? "You are following the caster." : "You are not following the caster"}
-        </div>
-      );
-    }
-
-    return new ImageResponse(
-      content,
-      {
-        width: 400,
-        height: 250,
-      }
+  if (page === "initial") {
+    content = (
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", backgroundColor: "#800080", color: "#fff", fontSize: 40 }}>
+        Welcome to Lucky Summer!
+        You can join the raffle if you follow the caster.
+      </div>
     );
-  } catch (error) {
-    console.error("Error generating image:", error);
-    return new Response("Internal Server Error", { status: 500 });
+  } else if (page === "result") {
+    content = (
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", backgroundColor: "#800080", color: "#fff", fontSize: 40 }}>
+        Transaction submitted! {transactionId}
+      </div>
+    );
+  } else {
+    content = (
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "100%", height: "100%", backgroundColor: "#800080", color: "#fff", fontSize: 40 }}>
+        {isFollowing ? "You are following the caster." : "You are not following the caster"}
+      </div>
+    );
   }
+
+  return new ImageResponse(
+    content,
+    {
+      width: 200,
+      height: 200,
+    }
+  );
 }
